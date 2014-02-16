ba5b6a66962361d48a1306586feb9e1bd2637694
danielnowendsztern <danielnowens@hotmail.com>
2014-02-16 16:44:53 +0100

    There are some problems wrapping the text content.

--- my-game/src/com/randombot/mygame/screens/Help.java
+++ my-game/src/com/randombot/mygame/screens/Help.java
@@ -10,14 +10,14 @@ public class Help extends BaseScreen {
 	final String instructionsText = "\n\nGame Instructions\n Lorem Ipsum es simplemente el texto de relleno de " +
 			"las imprentas y archivos de texto. Lorem Ipsum ha sido el texto " +
 			"de relleno estándar de las industrias desde el año 1500, cuando un " +
-			"impresor (N. del T. persona que se dedica a la imprenta) desconocido " +
+			"impresor (N. del T. persona que se dedica a la imprenta) desconocido." +
 			
 			
 			"\n\nGame Objectives\n usó una galería de textos y los mezcló de tal manera que logró hacer " +
 			"un libro de textos especimen. No sólo sobrevivió 500 años, sino que" +
 			" tambien ingresó como texto de relleno en documentos electrónicos, " +
 			"quedando esencialmente igual al original. Fue popularizado en los 60s " +
-			"con la creación de las hojas";
+			"con la creación de las hojas.";
 	
 	@Override
 	public void create() {
