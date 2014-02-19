06f5278c016d29167d9615d2f0548791e3a3a3a1
RotaruDan <drotaru@ucm.es>
2014-02-19 17:00:40 +0100

    Improved Help screen.

--- my-game/src/com/randombot/mygame/screens/Help.java
+++ my-game/src/com/randombot/mygame/screens/Help.java
@@ -1,19 +1,24 @@
 package com.randombot.mygame.screens;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 public class Help extends BaseScreen {
 	
-	final String instructionsText = "Game Instructions\n\n Lorem Ipsum es simplemente el texto de relleno de " +
+	final String descriptionTitle = "Game instructions";
+	final String descriptionText = "Lorem Ipsum es simplemente el texto de relleno de " +
 			"las imprentas y archivos de texto. Lorem Ipsum ha sido el texto " +
 			"de relleno est�ndar de las industrias desde el a�o 1500, cuando un " +
-			"impresor (N. del T. persona que se dedica a la imprenta) desconocido." +
-			
-			
-			"\n\nGame Objectives\n\n us� una galer�a de textos y los mezcl� de tal manera que logr� hacer " +
+			" tambien ingres� como texto de relleno en documentos electr�nicos, " +
+			"quedando esencialmente igual al original. Fue popularizado en los 60s " +
+			"impresor (N. del T. persona que se dedica a la imprenta) desconocido.";
+	
+	final String objectivesTitle = "Game objectives";
+	final String objectivesText = "Lorem Ipsum us� una galer�a de textos y los mezcl� de tal manera que logr� hacer " +
+			"las imprentas y archivos de texto. Lorem Ipsum ha sido el texto " +
+			"de relleno est�ndar de las industrias desde el a�o 1500, cuando un " +
 			"un libro de textos especimen. No s�lo sobrevivi� 500 a�os, sino que" +
 			" tambien ingres� como texto de relleno en documentos electr�nicos, " +
 			"quedando esencialmente igual al original. Fue popularizado en los 60s " +
@@ -24,15 +29,29 @@ public class Help extends BaseScreen {
 		super.create();
 		previousScreen = menu;
 		
-		root.defaults().fill().expand();
+		final Label descriptionTitleLabel = new Label(descriptionTitle, skin);
+		descriptionTitleLabel.setColor(Color.BLUE);	
+		final Label descriptionTextLabel = new Label(descriptionText, skin);
+		descriptionTextLabel.setWrap(true);
+		
+		final Label objectivesTitleLabel = new Label("\n" + objectivesTitle, skin);
+		objectivesTitleLabel.setColor(Color.BLUE);	
+		final Label objectivesTextLabel = new Label(objectivesText, skin);
+		objectivesTextLabel.setWrap(true);
+
+		final Table tableBody = new Table();
+		tableBody.defaults().fill().expand();
+		tableBody.add(descriptionTitleLabel);
+		tableBody.row();
+		tableBody.add(descriptionTextLabel);
+		tableBody.row();
+		tableBody.add(objectivesTitleLabel);
+		tableBody.row();
+		tableBody.add(objectivesTextLabel);
+		
+		final ScrollPane scroll = new ScrollPane(tableBody);		
+		root.add(scroll).fill().expand();
 		root.pad(0f, 30f, 0, 30f);
-		
-		final Label instructions = new Label(instructionsText, skin);
-		instructions.setWrap(true);
-		
-		final ScrollPane scroll = new ScrollPane(instructions);
-		
-		root.add(scroll);
 	}
 	
 //	@Override
