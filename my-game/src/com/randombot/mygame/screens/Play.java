a0c1ca736bbea97d42d925af8e98808458a2cb1b
RotaruDan <drotaru@ucm.es>
2014-02-21 17:09:50 +0100

    Changed pause window to dialog.

--- my-game/src/com/randombot/mygame/screens/Play.java
+++ my-game/src/com/randombot/mygame/screens/Play.java
@@ -4,10 +4,11 @@ import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.randombot.mygame.screens.transitions.Slice;
 
@@ -15,30 +16,31 @@ import com.randombot.mygame.screens.transitions.Slice;
 public class Play extends BaseScreen {
 	
 	private Label score;
+	private Dialog pauseMenu;
+	private boolean pause;
 
 	@Override
 	public void create() {
 		super.create();
 		previousScreen = menu;
 
-		root.defaults().fillX().expand();
+		root.defaults().fillX().expand().top();
 		score = new Label("Score: 0000000000000", skin);
 		score.setAlignment(Align.center);
 		root.add(score);
 		
-		final Window pauseMenu = new Window("Game Paused.", skin);
-		pauseMenu.setBounds(stage.getWidth()*0.2f, stage.getHeight()*0.2f, stage.getWidth()*0.6f, stage.getHeight()*0.5f);
+		pauseMenu = new Dialog("Game Paused.", skin, "exit-dialog");
 		pauseMenu.setResizable(false);
 		pauseMenu.setMovable(false);
-		pauseMenu.setVisible(false);		
+		pauseMenu.setModal(true);
 		pauseMenu.debug();
 
-		pauseMenu.defaults().fill().expand();
 		final TextButton resume = new TextButton("Continue", skin);
 		resume.addListener(new InputListener(){
 			@Override
 			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				pauseMenu.setVisible(false);
+				pauseMenu.hide();
+				pause = false;
 				return super.touchDown(event, x, y, pointer, button);
 			}		
 		});
@@ -47,34 +49,53 @@ public class Play extends BaseScreen {
 		exit.addListener(new InputListener(){
 			@Override
 			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				pauseMenu.setVisible(false);		
-				game.changeScreen(menu, Slice.init(1f, MathUtils.randomBoolean(), Slice.RANDOM, 10, Interpolation.sineOut));				
+				pauseMenu.hide();	
+				pauseMenu.setVisible(false);			
+				game.changeScreen(menu, Slice.init(1f, MathUtils.randomBoolean(), Slice.RANDOM, 10, Interpolation.sineOut));	
 				return super.touchDown(event, x, y, pointer, button);
 			}		
 		});
 		
-		pauseMenu.add(resume);
-		pauseMenu.row();
-		pauseMenu.add(exit);
+		Table buttonsTable = pauseMenu.getButtonTable();
+		float width = stage.getWidth()*.75f, height = stage.getHeight()*.25f;
+		buttonsTable.defaults().fill().expand();
+		buttonsTable.add(resume).width(width).height(height);
+		buttonsTable.row();
+		buttonsTable.add(exit).width(width).height(height);
 		
 		Image pauseButton = new Image(skin, "ic_more");
-		root.add(pauseButton).maxSize(50f);
+		root.add(pauseButton).maxSize(50f).padTop(10f);
 		pauseButton.addListener(new InputListener(){
 			@Override
 			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				pauseMenu.setVisible(!pauseMenu.isVisible());				
+				pauseMenu.setVisible(true);
+				pauseMenu.show(stage);		
+				pause = true;
 				return super.touchDown(event, x, y, pointer, button);
 			}		
 		});
 
 		root.row();
-		stage.addActor(pauseMenu);
 		
 		// Here start the game content 
 		
 		
 	}
 	
+	@Override
+	public void show() {
+		super.show();
+		pause = false;
+	}
+	
+	@Override
+	public void pause() {
+		pause = true;
+	}
 	
 	
+	@Override
+	public void resume() {
+		pauseMenu.show(stage);
+	}
 }
